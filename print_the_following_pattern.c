2277ddbc00955cdf4c4ccfdfa8188e7d7df42d06
Md shahil Haque <98087117+21P31A0599@users.noreply.github.com>
2022-02-05 21:17:57 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i==n||j==1||i==j)           { printf("*"); }            else{                printf(" ");            }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,11 +3,15 @@ int main()
 {
     int i,j,n;
     scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    for(i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        for(j=1;j<=n;j++)
         {
-           printf("%d",j); 
+            if(i==n||j==1||i==j)
+          { printf("*"); }
+           else{
+               printf(" ");
+           }
         }
         printf("
 ");
